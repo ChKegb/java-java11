15b17f6d2ced82b06daa00c120e01934afebf878
chkegb <kegbsuc@gmail.com>
2022-07-07 09:18:17 +0200

    Tp Stream

---
+++ src/test/java/java8/ex02/Method_02_Test.java
@@ -0,0 +1,67 @@
+package java8.ex02;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import java8.data.Data;
+import java8.data.Person;
+
+/**
+ * Exercice 02 - Redéfinition
+ */
+public class Method_02_Test {
+
+    // tag::IDao[]
+    interface IDao {
+        List<Person> findAll();
+
+        // TODO créer une méthode String format()
+        // TODO la méthode retourne une chaîne de la forme [<nb_personnes> persons]
+        // TODO exemple de résultat : "[14 persons]", "[30 persons]"
+        
+        default String format() {
+        	StringBuilder builder = new StringBuilder();
+        	return builder.append("[").append(findAll().size()).append( " persons]").toString();
+        }
+        
+       
+        
+        
+        
+        
+    }
+    // end::IDao[]
+
+    // tag::DaoA[]
+    class DaoA implements IDao {
+
+        List<Person> people = Data.buildPersonList(20);
+
+        @Override
+        public List<Person> findAll() {
+            return people;
+        }
+
+        // TODO redéfinir la méthode String format()
+        // TODO la méthode retourne une chaîne de la forme DaoA[<nb_personnes> persons]
+        // TODO exemple de résultat : "DaoA[14 persons]", "DaoA[30 persons]"
+        // TODO l'implémentation réutilise la méthode format() de l'interface
+        public String format() {
+        	StringBuilder builder = new StringBuilder();
+        	return builder.append(getClass().getSimpleName()).append(IDao.super.format()).toString();
+        }
+    }
+    // end::DaoA[]
+
+    @Test
+    public void test_daoA_format() throws Exception {
+
+        DaoA daoA = new DaoA();
+
+        // TODO invoquer la méthode format() pour que le test soit passant
+        String result = daoA.format();
+
+        assert "DaoA[20 persons]".equals(result);
+    }
+}
--- src/test/java/java8/ex03/Stream_03_Test.java
+++ src/test/java/java8/ex03/Stream_03_Test.java
@@ -29,7 +29,7 @@ public class Stream_03_Test {
 		List<Order> orders = new Data().getOrders();
 
 		// TODO Retrouver la commande avec le prix le plus élevé
-		Optional<Order> result = null;
+		Optional<Order> result = orders.stream().max((o1, o2) -> (int)o1.getPrice() - (int)o2.getPrice());;
 
 		assertThat(result.isPresent(), is(true));
 		assertThat(result.get().getPrice(), is(2200.0));
@@ -40,8 +40,8 @@ public class Stream_03_Test {
 
 		List<Order> orders = new Data().getOrders();
 
-		// TODO Retrouver la commande avec le prix le plus élevé
-		Optional<Order> result = null;
+		// TODO Retrouver la commande avec le prix le moins élevé
+		Optional<Order> result =orders.stream().min((o1, o2) -> (int)o1.getPrice() - (int)o2.getPrice());
 
 		assertThat(result.isPresent(), is(true));
 		assertThat(result.get().getPrice(), is(1000.0));
@@ -54,7 +54,7 @@ public class Stream_03_Test {
 
 		// TODO construire une chaîne contenant les prénoms des clients triés et séparés
 		// par le caractère "|"
-		String result = null;
+		String result = customers.stream().map(c -> c.getFirstname()).sorted().collect(Collectors.joining("|"));
 
 		assertThat(result, is("Alexandra|Cyril|Johnny|Marion|Sophie"));
 	}
@@ -76,7 +76,7 @@ public class Stream_03_Test {
 		List<Order> orders = new Data().getOrders();
 
 		// TODO Extraire la liste des différentes pizzas de toutes les commandes
-		List<Pizza> result = null;
+		List<Pizza> result = orders.stream().flatMap(o -> o.getPizzas().stream()).toList();
 
 		assertThat(result.size(), is(4));
 	}
@@ -87,7 +87,7 @@ public class Stream_03_Test {
 		List<Order> orders = new Data().getOrders();
 
 		// TODO construire une Map <Client, Commandes effectuées par le client
-		Map<Customer, List<Order>> result = null;
+		Map<Customer, List<Order>> result = orders.stream().collect(Collectors.groupingBy(o -> o.getCustomer()));
 
 		assertThat(result.size(), is(2));
 		assertThat(result.get(new Customer(1)), hasSize(4));
@@ -101,9 +101,10 @@ public class Stream_03_Test {
 		// TODO Séparer la liste des pizzas en 2 ensembles :
 		// TODO true -> les pizzas dont le nom commence par "L"
 		// TODO false -> les autres
-		Map<Boolean, List<Pizza>> result = null;
-
-		assertThat(result.get(true), hasSize(6));
+		Map<Boolean, List<Pizza>> result = pizzas.stream().collect(Collectors.partitioningBy(p->p.getName().startsWith(L)));
+				
+		
+				(result.get(true), hasSize(6));
 		assertThat(result.get(false), hasSize(2));
 	}
 }
